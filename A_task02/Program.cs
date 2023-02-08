b2a64524eb8b1d8d0b0b56339bef5106e1d3e586
Kseniya Elsukova <kseniyaels@yandex.ru>
2023-02-08 14:52:14 +0300

    Add a task02

---
+++ A_task02/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задайте одномерный массив из 123 случайных чисел, 
+найдите количетство элементов массива, значения которых лежат 
+в отрезке с 10 до 99*/
+
+int Count(int[] array, int min, int max)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] >= min) && (array[i] <= max))
+            result++;
+    }
+    return result;
+}
+
+int[] a = {10, 11, 12, 13, 14};
+Console.WriteLine(String.Join(" ", a));
+
+Console.WriteLine(Count(a, 10, 99));
